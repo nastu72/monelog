04b967c2d7b630016ce052832abe620069b016ce
nastu72 <65000342+nastu72@users.noreply.github.com>
2020-05-08 17:29:09 +0900

    Create head.php

---
+++ head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $siteTitle; ?>｜monelog</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC|Alegreya+Sans+SC|Noto+Sans+JP&display=swap" type="text/css">
+        <!-- フォントアイコン -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
